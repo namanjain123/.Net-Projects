191c9662f3a714cd8c1385545e54a9bdf702e485
namanjain123 <naman.nishi@gmail.com>
2022-12-06 16:23:19 +0530

    Azure Function Rest Api

---
+++ AzureFunctionRestApi/Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionRestApi
+{
+    class Model
+    {
+    }
+    public class Task
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+    public class TaskCreateModel
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+    public class TaskUpdateModel
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+
+}
