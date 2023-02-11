c14b8fb90802bdb1e2d884171e347fcd7c1250b4
namanjain123 <naman.nishi@gmail.com>
2023-02-11 21:39:36 +0530

    Addition of GraphQL based chart maker api

---
+++ Chart_Maker_GraphQL_API/ChartGraphQLAPI/Graph/Query.cs
@@ -0,0 +1,24 @@
+﻿using ChartGraphQLAPI.Logic;
+using ChartGraphQLAPI.Models;
+
+namespace ChartGraphQLAPI.Graph
+{
+    public class Query
+    {
+        public GraphsMaker gc;
+        public Charts GetChart(DataSource input) =>
+            new Charts
+            {
+               piechart=gc.Graph_Maker(input),
+               linechart= gc.Graph_Maker(input),
+               barchart= gc.Graph_Maker(input),
+               doublechart= gc.Graph_Maker(input),
+               radarchart=gc.Graph_Maker(input),
+               doughnutchart=gc.Graph_Maker(input),
+               polarchart= gc.Graph_Maker(input),
+               scatterchart = gc.Graph_Maker(input),
+               bubblechart = gc.Graph_Maker(input)
+
+            };
+    }
+}
