93c52eee060fdaa592b5a618b9173fd4bad2a02a
namanjain123 <naman.nishi@gmail.com>
2023-02-25 23:14:38 +0530

    addition of onion structure api

---
+++ OnionArchitecture/Domain Layer/Model/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer.Model
+{
+    public class Department : Base
+    {
+        public int Id {get;set;}   
+        public string DepName { get;set;}
+        public Student Students { get; set; }
+    
+    }
+}
---
+++ OnionArchitecture/Service Layer/DepartmentService.cs
@@ -0,0 +1,118 @@
+﻿using Domain_Layer.Model;
+using Repository_Layer.Interfaces;
+using Service_Layer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Layer
+{
+    public class DepartemntService: ICustomer<Department>
+    {
+        private readonly IRepository<Department> _studentRepository;
+        public DepartemntService(IRepository<Department> studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public void Delete(Department entity)
+        {
+            try
+            {
+                if (entity != null)
+                {
+                    _studentRepository.Delete(entity);
+                    _studentRepository.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public Department Get(int Id)
+        {
+            try
+            {
+                var obj = _studentRepository.Get(Id);
+                if (obj != null)
+                {
+                    return obj;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            try
+            {
+                var obj = _studentRepository.GetAll();
+                if (obj != null)
+                {
+                    return obj;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Insert(Department entity)
+        {
+            try
+            {
+                if (entity != null)
+                {
+                    _studentRepository.Insert(entity);
+                    _studentRepository.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Remove(Department entity)
+        {
+            try
+            {
+                if (entity != null)
+                {
+                    _studentRepository.Remove(entity);
+                    _studentRepository.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Update(Department entity)
+        {
+            try
+            {
+                if (entity != null)
+                {
+                    _studentRepository.Update(entity);
+                    _studentRepository.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
