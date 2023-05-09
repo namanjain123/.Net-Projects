df471c5368d59e3a4ce8aa65e280b37fec492abe
namanjain123 <naman.nishi@gmail.com>
2023-05-09 22:54:48 +0530

    addition of Web Socket

---
+++ WebSocket_API/WebSocket_API/WebSocket/WebSocketHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace WebSocket_API.WebSocket
+{
+    public class WebSocketHandler : WebSocketHandlerBase
+    {
+        public WebSocketHandler(WebSocket socket) : base(socket) { }
+
+        public override async Task OnOpen()
+        {
+            await SendStringAsync("Connection established.");
+        }
+
+        public override async Task OnMessage(string message)
+        {
+            await SendStringAsync($"Received: {message}");
+        }
+
+        public override async Task OnClose()
+        {
+            await SendStringAsync("Connection closed.");
+        }
+    }
+}
